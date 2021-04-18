f25f0d2f00525721d8b3849aa9ef29b3587fd229
ItsMeEniyan <57259374+ItsMeEniyan@users.noreply.github.com>
2021-04-19 00:03:00 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import JgLogo from "./statics/svg/pianoBg.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <div className="bgimg">
+        <div className="navbar">
+          <div>
+            <JgLogo />
+          </div>
+          <div className="navlinkgroup">
+            <div className="navlink">
+              Home
+            </div>
+            <div className="navlink">
+              About
+            </div>
+            <div className="navlink">
+              Work
+            </div>
+            <div className="navlink">
+              Contact
+            </div>
+
+          </div>
+
+        </div>
+
+      </div>
+      
     </div>
   );
 }
